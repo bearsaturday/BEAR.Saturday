ecd4bca068de58eb8fc00af093d4c8fe890a67d6
akihito <akihito.koriyama@gmail.com>
2011-06-25 10:23:32 +0900

    Add user age.

--- data/app/App/Ro/Test/User.php
+++ data/app/App/Ro/Test/User.php
@@ -39,8 +39,8 @@ class App_Ro_Test_User extends App_Ro
      * @var array
      */
     protected static $userData = array(
-        array('id' => 0, 'name' => 'World'),
-        array('id' => 1, 'name' => 'BEAR')
+        array('id' => 0, 'name' => 'World', 'age' => 1.37E10),
+        array('id' => 1, 'name' => 'BEAR', 'age' => 8)
     );
 
     /**
