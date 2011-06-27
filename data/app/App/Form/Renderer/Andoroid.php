0de9c04b4be674aa8790256686a9e6083b4d9c3c
Akihito Koriyama <akihito.koriyama@gmail.com>
2011-06-27 15:00:21 +0900

    Provide default empty UA renderer class.

--- data/app/App/Form/Renderer/Andoroid.php
+++ data/app/App/Form/Renderer/Andoroid.php
@@ -23,6 +23,6 @@
  * @link       http://@link_url@
  * @see       http://pear.php.net/package/HTML_QuickForm_Renderer_Tableless
  */
-class App_Form_Renderer_Default extends HTML_QuickForm_Renderer_Tableless
+class App_Form_Renderer_Iphone extends HTML_QuickForm_Renderer_Tableless
 {
 }
--- data/app/App/Form/Renderer/Mobile.php
+++ data/app/App/Form/Renderer/Mobile.php
@@ -1,69 +1,27 @@
 <?php
 /**
- * Replacement for the default renderer of HTML_QuickForm that uses only XHTML
- * and CSS but no table tags, and generates fully valid XHTML output
+ * App
  *
- * PHP versions 4 and 5
- *
- * LICENSE:
- *
- * Copyright (c) 2005-2007, Mark Wiesemann <wiesemann@php.net>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *    * Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *    * The names of the authors may not be used to endorse or promote products
- *      derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * @category   HTML
- * @package    HTML_QuickForm_Renderer_Tableless
- * @author     Alexey Borzov <borz_off@cs.msu.su>
- * @author     Adam Daniel <adaniel1@eesus.jnj.com>
- * @author     Bertrand Mansion <bmansion@mamasam.com>
- * @author     Mark Wiesemann <wiesemann@php.net>
- * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
- * @version    SVN: $Id:$
- * @link       http://pear.php.net/package/HTML_QuickForm_Renderer_Tableless
- * @ignore
+ * @category   BEAR
+ * @package    Bear.demo
+ * @subpackage Form
+ * @author     $Author:$ <username@example.com>
+ * @license    @license@ http://@license_url@
+ * @version    Release: @package_version@ $Id:$
+ * @link       http://@link_url@
  */
 
-
 /**
- * Replacement for the default renderer of HTML_QuickForm that uses only XHTML
- * and CSS but no table tags, and generates fully valid XHTML output
+ * App custom form
  *
- * You need to specify a stylesheet like the one that you find in
- * data/stylesheet.css to make this work.
- *
- * @category   HTML
- * @package    HTML_QuickForm_Renderer_Tableless
- * @author     Alexey Borzov <borz_off@cs.msu.su>
- * @author     Adam Daniel <adaniel1@eesus.jnj.com>
- * @author     Bertrand Mansion <bmansion@mamasam.com>
- * @author     Mark Wiesemann <wiesemann@php.net>
- * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
- * @version    Release: 0.6.1
- * @link       http://pear.php.net/package/HTML_QuickForm_Renderer_Tableless
- * @ignore
+ * @category   BEAR
+ * @package    Bear.demo
+ * @subpackage Form
+ * @author     $Author:$ <username@example.com>
+ * @license    @license@ http://@license_url@
+ * @version    Release: @package_version@ $Id:$
+ * @link       http://@link_url@
+ * @see       http://pear.php.net/package/HTML_QuickForm_Renderer_Tableless
  */
 class App_Form_Renderer_Mobile extends HTML_QuickForm_Renderer_Tableless
 {
@@ -114,7 +72,8 @@ class App_Form_Renderer_Mobile extends HTML_QuickForm_Renderer_Tableless
      * @var      string
      * @access   private
      */
-    private $_requiredNoteTemplate = "\n\t\t\t<li class=\"reqnote\"><label class=\"element\">&nbsp;</label>{requiredNote}</li>";
+    private $_requiredNoteTemplate
+        = "\n\t\t\t<li class=\"reqnote\"><label class=\"element\">&nbsp;</label>{requiredNote}</li>";
 
     /**
      * Constructor
