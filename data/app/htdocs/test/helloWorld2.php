f4a34a3f4d00a92caaf81bdf566febd0687dab1d
Akihito Koriyama <akihito.koriyama@gmail.com>
2011-06-30 11:15:35 +0900

    Add correct helloWorld2.php.

---
+++ data/app/htdocs/test/helloWorld2.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * App
+ *
+ * @category   BEAR
+ * @package    BEAR.app
+ * @subpackage Page
+ * @author     $Author:$ <username@example.com>
+ * @license    @license@ http://@license_url@
+ * @version    Release: @package_version@ $Id:$
+ * @link       http://@link_url@
+ */
+
+require_once 'App.php';
+
+/**
+ * Hello World 2
+ *
+ * @category   BEAR
+ * @package    BEAR.app
+ * @subpackage Page
+ * @author     $Author:$ <username@example.com>
+ * @license    @license@ http://@license_url@
+ * @version    Release: @package_version@ $Id:$
+ * @link       http://@link_url@
+ */
+class Page_Test_HelloWorld2 extends App_Page
+{
+    /**
+     * Inject
+     *
+     * @return void
+     */
+    public function onInject()
+    {
+        parent::onInject();
+        $this->injectGet('id', 'id', 0);
+    }
+
+    /**
+     * Init
+     *
+     * @param array $args
+     *
+     * @return void
+     */
+    public function onInit(array $args)
+    {
+        $params = array(
+            'uri' => 'Test/User',
+            'values' => array('id' => $args['id']),
+            'options' => array('template' => 'test/greeting')
+        );
+        $this->_resource->read($params)->set('greeting');
+    }
+
+    /**
+     * Output
+     *
+     * @return void
+     */
+    public function onOutput()
+    {
+        $this->display('helloWorld.tpl');
+    }
+}
+
+App_Main::run('Page_Test_HelloWorld2', array(), array('injector' => 'onInjectUA'));
