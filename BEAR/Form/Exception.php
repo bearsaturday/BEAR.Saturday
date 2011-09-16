44309f6c8d8226016aa9fd149ea960642d5e0e7a
Akihito Koriyama <akihito.koriyama@gmail.com>
2011-09-16 16:34:44 +0900

    Add BEAR_Form_Exception

---
+++ BEAR/Form/Exception.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * BEAR
+ *
+ * PHP versions 5
+ *
+ * @category   BEAR
+ * @package    BEAR_Form
+ * @author     Akihito Koriyama <koriyama@bear-project.net>
+ * @copyright  2008-2011 Akihito Koriyama All rights reserved.
+ * @license    http://opensource.org/licenses/bsd-license.php BSD
+ * @version    SVN: Release: @package_version@ $Id: $
+ * @link      http://www.bear-project.net/
+ */
+
+/**
+ * BEAR_Form_Exception
+ *
+ * @category   BEAR
+ * @package    BEAR_Form
+ * @author     Akihito Koriyama <koriyama@bear-project.net>
+ * @copyright  2008-2011 Akihito Koriyama All rights reserved.
+ * @license    http://opensource.org/licenses/bsd-license.php BSD
+ * @version    Release: @package_version@ $Id:$
+ * @link       http://www.bear-project.net
+ */
+class BEAR_Form_Exception extends BEAR_Exception
+{
+}
