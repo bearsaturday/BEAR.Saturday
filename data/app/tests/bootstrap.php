fa2d03535e6c2283529729d0ba840bba433811b2
YAMANE Nana <shigematsu.nana@gmail.com>
2013-07-11 21:58:17 +0900

    change test skeleton, autoloader, add utility

--- data/app/tests/bootstrap.php
+++ data/app/tests/bootstrap.php
@@ -1,16 +1,17 @@
 <?php
-// set path
+// for test
+$_SERVER['bearmode'] = 100;
+
+$loader = require dirname(__DIR__). '/vendor/autoload.php';
+/** @var $loader \Composer\Autoload\ClassLoader */
+$loader->add('', __DIR__);
+$loader->register();
+
 $appPath = realpath(__DIR__ . '/..');
-// set autoloder
-set_include_path($appPath . PATH_SEPARATOR . get_include_path());
+set_include_path($appPath . PATH_SEPARATOR . $appPath . '/libs/pear/php' . PATH_SEPARATOR . $appPath  . '/libs/pear/php/BEAR' . PATH_SEPARATOR . $appPath . '/libs/pear/php/BEAR/vendors/PEAR' . PATH_SEPARATOR . get_include_path());
+
 require_once $appPath . '/App.php';
 
-spl_autoload_register('bearTestAutolodaer');
-
-$filter = PHP_CodeCoverage_Filter::getInstance();
-$filter->removeDirectoryFromWhitelist($appPath . 'App/views');
-
-function bearTestAutolodaer($class) {
-    $file = str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
-    require_once $file;
-}
+// extension Xdebug
+//$filter = PHP_CodeCoverage_Filter::getInstance();
+//$filter->removeDirectoryFromWhitelist($appPath . 'App/views');
