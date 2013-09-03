c3e1950363c1e6b8604c7619997b40d2c2c264a6
YAMANE Nana <shigematsu.nana@gmail.com>
2013-09-03 11:02:15 +0900

    change app for test

--- data/app/App/Ro/Test/User.php
+++ data/app/App/Ro/Test/User.php
@@ -1,41 +1,8 @@
 <?php
-/**
- * App
- *
- * @category   BEAR
- * @package    BEAR.app
- * @subpackage Ro
- * @author     $Author:$ <username@example.com>
- * @license    @license@ http://@license_url@
- * @version    Release: @package_version@ $Id:$
- * @link       http://@link_url@
- */
 
-/**
- * User
- *
- * @category   BEAR
- * @package    BEAR.app
- * @subpackage Ro
- * @author     $Author:$ <username@example.com>
- * @license    @license@ http://@license_url@
- * @version    Release: @package_version@ $Id:$
- * @link       http://@link_url@
- */
 class App_Ro_Test_User extends App_Ro
 {
     /**
-     * Inject
-     *
-     * @return void
-     */
-    public function onInject()
-    {
-    }
-
-    /**
-     * data
-     *
      * @var array
      */
     protected static $userData = array(
@@ -54,6 +21,7 @@ class App_Ro_Test_User extends App_Ro
     {
         $id = $values['id'];
         $this->assert(isset(self::$userData[$id]));
+
         return self::$userData[$id];
     }
 
