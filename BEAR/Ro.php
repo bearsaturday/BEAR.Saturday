4a0d3466c5a9b716a01370152848da51e51c8fd6
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-01-28 13:16:31 +0900

    fix @var

--- BEAR/Ro.php
+++ BEAR/Ro.php
@@ -773,7 +773,7 @@ class BEAR_Ro extends ArrayObject implements IteratorAggregate, BEAR_Ro_Interfac
         }
         // ページ
         $page = BEAR::dependency('App_Page', self::$_page);
-        /* @var $page App_Page */
+        /* @var $page BEAR_Page */
         $val = (isset($config['ro']) && $config['ro'] === true) ? $this : $this->_body;
         $page->set($key, $val);
 
